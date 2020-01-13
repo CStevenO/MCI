42c07e217ef72d2123ae69c1f0e1a2af7a9262e2
Steven <53093183+CStevenO@users.noreply.github.com>
2020-01-13 10:11:15 -0500

    Archivos Subidos

---
+++ NANO_AM2305_SD_fat/NANO_AM2305_SD_fat.ino
@@ -0,0 +1,17 @@
+#include "Def.h"
+void setup() 
+{
+  wdt_disable();
+  #include "Conf.h"
+  wdt_enable(WDTO_4S);
+
+}
+
+void loop() 
+{
+  tiempoActual(); //RTC
+  obtenerVariables(); //AM2305
+  principal(); //MENU
+  enviarDatos(); //RTC
+  watchdogSetup(); //perro guardian
+}
