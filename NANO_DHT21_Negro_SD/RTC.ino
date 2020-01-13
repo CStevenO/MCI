23af0f28be621bed9f7ac293a1cd4ecd8251109c
Steven <53093183+CStevenO@users.noreply.github.com>
2020-01-13 10:30:54 -0500

    Archivos Subidos

---
+++ NANO_DHT21_Negro_SD/RTC.ino
@@ -0,0 +1,126 @@
+void tiempoActual() 
+{
+  DateTime now = RTC.now(); // Obtiene la fecha y hora del RTC
+  anio = now.year();
+  mes = now.month();
+  dia = now.day();
+  hora = now.hour();
+  minuto =  now.minute();
+  segundo = now.second();
+}
+
+void enviarDatos() {
+ 
+  if (esp.available()>0) 
+  {
+//    mensajeEntrada.reserve(5);
+    mensajeEntrada = esp.readStringUntil(';');
+    idMensaje = mensajeEntrada.substring(1).toInt();
+    idMen = mensajeEntrada.substring(4).toInt();
+    lcd.setCursor(14, 1);
+    lcd.print(idMensaje); 
+    lcd.setCursor(16, 1);
+    lcd.print(idMen);   
+    
+    switch(idMensaje)
+    {
+    case 0:
+      //      mensajeS.reserve(40);
+      //      mensajeSalida = "01," + String(anio) + "-" + String(mes) + "-" + String(dia) + " " + String(hora) + ":" + String(minuto) + ":" + String(segundo) + "," + idInvernadero + "," + String(t, 2) + "," + String(h, 2) + ";";
+              
+              switch(idMen)
+              {
+                case 0:
+
+                  lcd.setCursor(9, 1);
+                  lcd.print("N"); 
+                  lcd.print(" ");
+                  lcd.print(minuto);           
+
+                  if(minuto!=minutoant && minuto % intervalo == 0)  //guarda un archivo no enviado solo del minuto del intervalo
+                  {
+                    minutoant=minuto;
+                    myFile = SD.open("Datos.txt", FILE_WRITE);//abrimos  el archivo
+                    if (myFile) 
+                   {
+                    // mensajeSalida= mensajeSalida + "&";
+                     myFile.print(mensajeSalida);
+                     myFile.close();
+               
+                    }
+                  }
+                  
+                break;
+
+                case 1:
+                
+                  lcd.setCursor(9, 1);
+                  lcd.print("S"); 
+                  lcd.print(" ");
+                  lcd.print(minuto); 
+                  myFile = SD.open("Datos.txt");
+                  int peso = myFile.size();//guarda el tamaño del fichero
+                  
+                  if(myFile)
+                  {
+                    if(UltimaPosicion>=peso)   
+                    
+                    {
+                      UltimaPosicion=0;
+                      SD.remove("Datos.txt");
+                    }
+                    myFile.seek(UltimaPosicion);
+                    
+                    while(myFile.available())
+                   {
+                    mensajeGuardado = myFile.read();
+                    UltimaPosicion = myFile.position();
+                    esp.print(mensajeGuardado);
+                    if(mensajeGuardado==59)//ASCII de &
+                     {            
+                      break;
+                     }
+                   }
+                  
+                   myFile.close();
+                  }
+             
+                break;
+              }
+                         
+    break;
+    
+    case 3:
+    
+      mensajeEntrada = mensajeEntrada.substring(3);
+      for (i = 0; i < 6 ; i++)
+      {
+        indice = mensajeEntrada.indexOf(separador);
+        datos[i] = (mensajeEntrada.substring(0, indice)).toInt();
+        mensajeEntrada = mensajeEntrada.substring(indice + 1);
+      }
+      RTC.adjust(DateTime(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]));
+    
+    case 4:
+        //enciende efectores
+
+    break;
+    }
+      
+   }
+  if (minuto % intervalo == 0 && segundo == 0 && !unicaVez && h > 0 && t > 0 ) 
+  {
+    mensajeSalida.reserve(40);
+    mensajeSalida = "01," + String(anio) + "-" + String(mes) + "-" + String(dia) + " " + String(hora) + ":" + String(minuto) + ":" + String(segundo) + "," + idInvernadero + "," + String(t, 2) + "," + String(h, 2) + ";";
+    //mensajeSalida =  idInvernadero + "," + String(h, 2) + "," + String(t, 2) + ";";
+    lcd.clear();
+    unicaVez = true;
+    esp.print(mensajeSalida);
+  }
+
+    
+  if (minuto % intervalo != 0) 
+  {
+    unicaVez = false;
+  }
+}
