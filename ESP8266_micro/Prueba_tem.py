a8004b642ca1a6649d555b9d6c9828b5582f1338
Diany Lorena Hincapie Melo <pastmtto@eliteflower.com>
2020-07-04 08:16:36 -0500

    no se

--- ESP8266_micro/Prueba_tem.py
+++ ESP8266_micro/Prueba_tem.py
@@ -19,17 +19,38 @@ while True:
         elif data is not None:
             sr=sr+data
 """
+laser = Pin(25, Pin.OUT)
+
 def conexion():
-    i2c = I2C(scl=Pin(22), sda=Pin(21),freq=50000)
-    sensor = mlx90614.MLX90614(i2c)
+    try:
+        i2c = I2C(scl=Pin(22), sda=Pin(21),freq=50000)
+        sensor = mlx90614.MLX90614(i2c)
+    except:
+        print("conectando...")
+        time.sleep(2)
+        conexion()
 
-try:
-    conexion()
-except:
-    print("conectando...")
-    time.sleep(2)
-    conexion()
+def read_tem():
+    try:
+        laser.value(1)
+        while sensor.read_object_temp() <= 25:
+            pass
+        time.sleep(0.5)
+        prom = 0
+        for i in range(40):
+            prom = prom + sensor.read_object_temp()
+            time.sleep_ms(25)
+        prom = prom/40
+        laser.value(0)
+        return prom
+    except:
+        print("Error")
+        return 0
 
+conexion()
 while True:
-    print(sensor.read_object_temp()*0.535+12)
-    time.sleep_ms(50)
+    x = read_tem()
+    print("medida mejorada: ", 1.4424*x-0.0154*x*x+2.2569)
+    print("medida cruda: ", x)
+    #print(sensor.read_object_temp()*0.535+12)
+    time.sleep(1)
